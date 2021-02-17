b6fc03e0271ea85699ecaae2ee177262bea35442
Roman Sorin <roman@romansorin.com>
2021-02-17 11:43:15 -0500

    Begin ticketing script

---
+++ ticket.py
@@ -0,0 +1,13 @@
+###
+# Quick textual generation of citations based on citation format within Ohio (Lake County)
+# Creates similar outline to real citations from input and numerical selections to speed up writing process in-game.
+###
+
+# As a user, I want to be able to run the script and then provide relevant information regarding the civilian and offenses,
+# and then have the full format returned to me when I cease execution.
+# I want to be able to provide the time and date of the offense, the vehicle information, and where the offense occurred.
+# I want to select between speed, OVI, driver license infraction, safety belt, or other offenses, and provide the relevant information for each.
+# I would like to be able to mark additional information such as holding driver's license or seizing vehicle.
+# I would like to be able to list the conditions of the pavement, weather, area, etc.
+# I would like to be able to add remarks in textual format.
+# I would like to be able to mark if any criminal charges (felony) occurred and the number of offenses committed.
